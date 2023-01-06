c2fae19294d68468eecc15e982db712acc3c30a8
Alander Gimenez <82914806+alandergimenez@users.noreply.github.com>
2023-01-06 22:46:05 +0100

    Eliminando errores fase 1

--- js/script.js
+++ js/script.js
@@ -23,38 +23,3 @@ $( ()=>{
 
 
 } )
-
-
-
-
-
-
-/* codigo en javascript de la clase anterior
-function campo(orden){
-    if (orden == 1){
-        let lbl_nombre = document.getElementById("lblnombre");
-        lbl_nombre.style.color = "red";
-    }
-    
-    if (orden == 2){
-        let lbl_apellidos = document.getElementById("lblapellidos");
-        lbl_apellidos.style.color = "red";
-    }
-}
-
-document.getElementById("form_pedidos").addEventListener("submit", 
-    function(event){
-        if ( document.getElementById("nombre").value == "" ) {
-            event.preventDefault();
-            campo(1);
-        }
-
-        if ( document.getElementById("apellidos").value == "" ) {
-            event.preventDefault();
-            campo(2);
-        }
-    }
-);
-
-*/
-
