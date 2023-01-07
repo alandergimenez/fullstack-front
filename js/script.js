18afe7719ed88920ec4acaed0c645777d021a768
Alander Gimenez <82914806+alandergimenez@users.noreply.github.com>
2023-01-07 16:21:15 +0100

    Frontend Pipeline Trabajo final

--- js/script.js
+++ js/script.js
@@ -21,5 +21,30 @@ $( ()=>{
         }
     );
 
+    $("#tamano").on('change', function() { 
+        console.log(this.value); 
+        let size = this.value;
+
+        $.ajax({
+
+            url : 'http://localhost:5000/checksize',
+
+            data : { tamano : size },
+
+            type : 'POST',
+
+            success : function(xhr, status) {
+                console.log(xhr)
+                $('#disponibilidad').text(xhr);
+            },
+
+            error : function(xhr, status) {
+                console.log(xhr)
+                $('#disponibilidad').text(xhr);
+            }
+
+        })
+    });
+
 
 } )
